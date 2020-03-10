d4cdb92207193ce31fcf6679c988173b2dd29d6e
Sam Palmer <github@sampalmer.dev>
2020-03-10 16:15:17 +0000

    Return result as json

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import yara
 import boto3
+import json
 
 
 def analyze_lambda_handler(event, lambda_context):
@@ -12,4 +13,4 @@ def analyze_lambda_handler(event, lambda_context):
     s3 = boto3.client("s3")
     s3_object = s3.get_object(Bucket=bucket, Key=key)
     streaming_body = s3_object["Body"]
-    return rules.match(data=streaming_body.read())
+    return json.dumps({"result": rules.match(data=streaming_body.read())})
