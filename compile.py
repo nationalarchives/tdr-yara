4e69f3f7e1c08ee7d274c317625efadbea01aa28
Sam Palmer <github@sampalmer.dev>
2020-03-10 13:13:45 +0000

    Walk through all subdirectories to find files

--- compile.py
+++ compile.py
@@ -1,8 +1,23 @@
 #! /usr/bin/python3
 import yara
-import glob
 
-rules_files = {path: path for path in glob.glob("yar/*.yar*")}
+import os
+
+RULES_DIR = "."
+
+
+def get_rules_files():
+    for root, s, files in os.walk(RULES_DIR):
+        for filename in files:
+            lower_filename = filename.lower()
+            if lower_filename.endswith('.yar') or lower_filename.endswith('.yara'):
+                yield os.path.relpath(os.path.join(root, filename), start=RULES_DIR)
+
+
+yara_rules = get_rules_files()
+rules_files = {relative_path: os.path.join(RULES_DIR, relative_path)
+               for relative_path in yara_rules}
+
 rules = yara.compile(filepaths=rules_files,
-                     externals={'extension': '', 'filename': '', 'filepath': '', 'filetype': '', 'certificate': ''})
+                     externals={'extension': '', 'filename': '', 'filepath': '', 'filetype': ''})
 rules.save("/output")
